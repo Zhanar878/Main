c38defa678cb9ee82fa66fc9e49a69568d7ebd5c
Zhanar <zh.dosymbekova@gmail.com>
2022-11-23 00:39:07 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,111 @@
+public class Main {
+    public static void main(String[] args) {
+     /*double e=20/2.5;
+        System.out.println("22.5/2.5="+e);
+
+*/
+
+
+
+
+      /*  int a=10;
+int b=5;
+
+int c=a*b;
+
+System.out.println(a+"*"+b+"="+c);
+*/
+/*
+c=a-b;
+d=4-a;
+System.out.println(a+"-"+b+"="+c);
+System.out.println(4+"-"+a+"="+d);
+*/
+        /*
+int c=a*b;
+int d=b*5;
+System.out.println(a+"*"+b+"="+c);
+System.out.println(b+"*"+5+"="+d);
+
+c=a/b;
+double e=22.5/4.5;
+System.out.println(a+"/"+b+"="+c);
+System.out.println("22.5/4.5="+e);
+
+*/
+
+    /*    int a=25;
+        a++;
+        System.out.println("a++="+a);
+        System.out.println("a++="+a++);
+        a--;
+        System.out.println("a--="+a);
+        System.out.println("a--="+a--);
+*/
+/*
+
+        int a=25;
+        ++a;
+        System.out.println("++a="+a);
+        System.out.println("++a="+ ++a);
+        --a;
+        System.out.println("--a="+a);
+        System.out.println("--a="+ --a);
+
+*/
+/*
+        int a=5;
+        System.out.println("a+10:"+(a+=10));
+        System.out.println("a-=3:"+(a-=3));
+        System.out.println("a*=2:"+(a*=2));
+        System.out.println("a/=6:"+(a/=6));
+        System.out.println("a<<=4:"+(a<<=4));
+        System.out.println("a>>=2:"+(a>>=2));
+*/
+
+/*
+        int a=10;
+        int b=4;
+        System.out.println(a+"=="+b+":"+(a==b));
+        System.out.println(a+"==10:"+(a==10));
+     System.out.println(a+"!="+b+":"+(a!=b));
+     System.out.println(a+"!="+b+":"+(a!=b));
+     System.out.println(a+"<"+b+":"+(a<b));
+     System.out.println(a+">"+b+":"+(a>b));
+       /* System.out.println("10>=10:"+(10>=10));
+        System.out.println("10>=4:"+(10>=4));
+        System.out.println("10>=20:"+(10>=20));
+        System.out.println("10<=10:"+(10<=10));
+        System.out.println("10<=4:"+(10<=4));
+        System.out.println("10<=20:"+(10<=20));
+*/
+/*
+boolean a1=(5>6)||(4<6);
+        boolean a2=(5>6)||(4<6);
+        boolean a3=(5>6)&&(4<6);
+        boolean a4=(50>6)&&(4/2<3);
+        boolean a5=(5<6)^(4>6);
+        boolean a6=(50>6)||(4/2<3);
+
+        System.out.println("5>6: "+(5>6)+", 4<6:"+(4<6)+", результат: "+a1);
+        System.out.println("5>6: "+(5>6)+", 4>6:"+(4>6)+", результат: "+a2);
+        System.out.println("5>6: "+(5>6)+", 4<6:"+(4<6)+", результат: "+a3);
+        System.out.println("50>6: "+(50>6)+", 4/2<3:"+(4/2<3)+", результат: "+a4);
+        System.out.println("5<6: "+(5<6)+", 4>6:"+(4>6)+", результат: "+a5);
+        System.out.println("50>6: "+(50>6)+", 4/2<3:"+(4/2<3)+", результат: "+a6);
+
+*/
+       /* int a = 24;
+        int b = -5;
+        System.out.println(a%b);*/
+
+       double Hello;
+
+       double d1=2.5;
+
+       System.out.println(d1);
+
+
+
+    }
+}
